eb01875b3ce7b77dd045ea27667dcc4de7f83cd9
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-27 16:36:35 +0900

    adding the monaco editor + handling editor changes

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,13 +2,30 @@
 
 import { Snippet } from '@prisma/client';
 import React from 'react';
+import { Editor } from '@monaco-editor/react';
 
 type Props = {
     snippet: Snippet;
 };
 
 const SnippetEditForm = ({ snippet }: Props) => {
-    return <div>{snippet.title}</div>;
+    const [code, setCode] = React.useState(snippet.code);
+
+    const handleEditorChange = (value: string = '') => {
+        setCode(value);
+    };
+    return (
+        <div>
+            <Editor
+                height="40vh"
+                theme="vs-dark"
+                defaultLanguage="javascript"
+                defaultValue={code}
+                options={{ minimap: { enabled: false } }}
+                onChange={handleEditorChange}
+            />
+        </div>
+    );
 };
 
 export default SnippetEditForm;
