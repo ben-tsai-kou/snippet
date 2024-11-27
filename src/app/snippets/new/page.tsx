bf919eca06278deb2a861ae57d8bd58bc45227c3
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-27 10:20:09 +0900

    adding a creation form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,3 +1,34 @@
 export default function CreateSnippetPage() {
-    return <div>create snippet</div>;
+    return (
+        <form>
+            <h3 className="font-bold m-3">Create a Snippet</h3>
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-4">
+                    <label htmlFor="title" className="w-12">
+                        Title
+                    </label>
+                    <input
+                        name="title"
+                        className="border rounded p-2 w-full"
+                        id="title"
+                    />
+                </div>
+
+                <div className="flex gap-4">
+                    <label htmlFor="code" className="w-12">
+                        Code
+                    </label>
+                    <textarea
+                        className="border rounded p-2 w-full"
+                        id="code"
+                        name="code"
+                    />
+                </div>
+
+                <button type="submit" className="rounded p-2 bg-blue-200">
+                    Create
+                </button>
+            </div>
+        </form>
+    );
 }
