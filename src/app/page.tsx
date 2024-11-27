1dab7082755ce33fbf76d7fa8ed6cb560882f04a
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-27 12:10:27 +0900

    fetching data with server components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-    return <div>hello world</div>;
+import { db } from '@/db';
+
+export default async function Home() {
+    const response = await db.snippet.findMany();
+
+    const snippet = response.map((snippet) => {
+        return (
+            <div key={snippet.id}>
+                <h1>{snippet.title}</h1>
+                <p>{snippet.code}</p>
+            </div>
+        );
+    });
+
+    return <div>{snippet}</div>;
 }
