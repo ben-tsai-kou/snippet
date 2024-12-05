a5325306365df5178f3c0dd46806c7db2b070866
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-12-05 09:57:09 +0900

    gotchas around error handling in nextjs

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -26,34 +26,48 @@ export const createSnippet = async (
     },
     formData: FormData
 ) => {
-    // check the user's inputs and make sure they're valid
-    const title = formData.get('title') || '';
-    const code = formData.get('code') || '';
+    try {
+        // check the user's inputs and make sure they're valid
+        const title = formData.get('title');
+        const code = formData.get('code');
 
-    if (typeof title !== 'string' || title.length < 3) {
-        return {
-            title: typeof title === 'string' ? title : '',
-            code: typeof code === 'string' ? code : '',
-            message: 'Title must be longer',
-        };
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                title: typeof title === 'string' ? title : '',
+                code: typeof code === 'string' ? code : '',
+                message: 'Title must be longer',
+            };
+        }
+
+        if (typeof code !== 'string' || code.length < 3) {
+            return {
+                title: typeof title === 'string' ? title : '',
+                code: typeof code === 'string' ? code : '',
+                message: 'Code must be longer',
+            };
+        }
+
+        // create a new record in the database
+        await db.snippet.create({
+            data: {
+                title,
+                code,
+            },
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                title: '',
+                code: '',
+                message: err.message,
+            };
+        } else {
+            return {
+                title: '',
+                code: '',
+                message: 'An error occurred',
+            };
+        }
     }
-
-    if (typeof code !== 'string' || code.length < 3) {
-        return {
-            title: typeof title === 'string' ? title : '',
-            code: typeof code === 'string' ? code : '',
-            message: 'Code must be longer',
-        };
-    }
-
-    // create a new record in the database
-    await db.snippet.create({
-        data: {
-            title,
-            code,
-        },
-    });
-
-    // redirect the user back to the root route
     redirect('/');
 };
